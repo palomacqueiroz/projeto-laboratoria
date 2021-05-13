cf645556bef79f1e29a6e2fed17b91ad5ce5458b
Paloma Queiroz <palomacqueiroz@hotmail.com>
2021-05-13 15:57:42 -0300

    atualização Js e Html

--- main.js
+++ main.js
@@ -2,17 +2,16 @@
 const nome = prompt("Qual seu nome ?");
 const novoNome = document.getElementById("nome");
 novoNome.innerHTML= "Olá " + nome + " vamos jogar!"
-//novoNome.innerHTML= "OláBlá "  
 
 
-let inicioJogo = prompt("Selecione uma opção: \n Você gostaria de jogar? \n 1. Sim \n 2.Não " );
+let inicioJogo = prompt("Selecione uma opção: \n Por favor, selecionar apenas números 1 ou 2. \n Você gostaria de jogar? \n 1. Sim \n 2.Não " );
 
 if (inicioJogo == 1) {
-    alert ("Vamos iniciar");
+    alert ("Vamos iniciar" \n "Lembre-se de selecionar apenas a letra correta: a, b ou c");
 
     const primeiraPergunta = prompt("1.Qual a capital do Brasil? \n a)Brasília \n b)Salvador \n c)Rio de Janeiro");
     const capitalBrasil = document.getElementById("primeiraPergunta");
-    capitalBrasil.innerHTML= "b"
+    capitalBrasil.innerHTML= "a"
 
     const segundaPergunta = prompt("2.Qual a capital do Peru? \n a)Cusco \n b)Lima \n c)Trujillo");
     //const capitalPeru = document.getElementById("segundaPergunta");
